3a6d1cc56d546fcc63a8b2438462c58fa8a24f0f
Nikolai <nlv.nki@gmail.com>
2018-12-21 00:29:14 +0300

    map was implemented, bx-slider fix image height size

--- sourse/js/script.js
+++ sourse/js/script.js
@@ -11,8 +11,27 @@ $(document).ready(function(){
   });
 
 
+
+  $('.modal-map-open').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		modal: true
+	});
+
+
+  $(document).on('click', '.popup-modal__close', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+
+
+
+
+
+
+
  let param = {
-   auto:true,
+   auto:false,
    slideWidth: 300,
    pager: false,
    nextText:'Вперед',
@@ -26,18 +45,18 @@ $(document).ready(function(){
   $('.slider').bxSlider(param);
 
 
-  $('#image-gallery').lightSlider({
-      gallery:true,
-      item:1,
-      thumbItem:3,
-      slideMargin: 0,
-      speed:500,
-      auto:false,
-      loop:true,
-      onSliderLoad: function() {
-          $('#image-gallery').removeClass('cS-hidden');
-      }
-  });
+  // $('#image-gallery').lightSlider({
+  //     gallery:true,
+  //     item:1,
+  //     thumbItem:3,
+  //     slideMargin: 0,
+  //     speed:500,
+  //     auto:false,
+  //     loop:true,
+  //     onSliderLoad: function() {
+  //         $('#image-gallery').removeClass('cS-hidden');
+  //     }
+  // });
 
 
 
