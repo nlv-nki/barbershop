028a1277f808a213a291c4c37bc66f50dafe8e35
Nikolai <nlv.nki@gmail.com>
2018-12-24 16:53:45 +0300

    burgerr menu was implemented on index

--- sourse/js/script.js
+++ sourse/js/script.js
@@ -43,6 +43,12 @@ $(document).ready(function(){
 
 
 
+	jQuery('.main-nav__toggle-nav').click(function(e) {
+		jQuery(this).toggleClass('active');
+		jQuery('.main-nav ul').toggleClass('active');
+
+		e.preventDefault();
+	});
 
 
 });
